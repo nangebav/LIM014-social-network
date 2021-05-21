b2c633c21ad44f74ab1ea24073b7ea15af0f3133
nangebav <nangebav@gmail.com>
2021-05-20 20:10:42 -0500

    test

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,15 +2,31 @@
 import { signInFunction, signInGoogle } from '../src/controller-function/auth-logIn.js';
 import { sendEmail, registrationFunction } from '../src/controller-function/auth-register.js';
 
+const firebasemock = require('firebase-mock');
+
+const mockauth = new firebasemock.MockAuthentication();
+// const mockdatabase = new firebasemock.MockFirebase();
+const mockfirestore = new firebasemock.MockFirestore();
+const mockstorage = new firebasemock.MockStorage();
+// const mockmessaging = new firebasemock.MockMessaging();
+const ocksdkm = new firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  // (path) => (path ? mockdatabase.child(path) : mockdatabase),
+  null,
+  // use null if your code does not use AUTHENTICATION
+  () => mockauth,
+  // use null if your code does not use FIRESTORE
+  () => mockfirestore,
+  // use null if your code does not use STORAGE
+  () => mockstorage,
+  // use null if your code does not use MESSAGING
+  // () => mockmessaging,
+  null,
+);
+ocksdkm();
+
 describe('signInFunction', () => {
   it('Debería poder iniciar sesión con email: lemeca5029@o3live.com y password:abcdefgh', () => {
-    expect(signInFunction('lemeca5029@o3live.com', 'abcdefgh'))
-      .then((user) => {
-        expect(user.email).toBe('lemeca5029@o3live.com');
-      });
-  });
-
-  it('No debería poder iniciar sesión con email: lemeca5029@o3live.com y password:abcdefgh', () => {
     expect(signInFunction('lemeca5029@o3live.com', 'abcdefgh'))
       .then((user) => {
         expect(user.email).toBe('lemeca5029@o3live.com');
