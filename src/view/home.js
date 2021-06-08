4dbcd8f13bf08392aa35cb45585afa9b2bf1fe0a
gabrielaguerrac <gguerracast@gmail.com>
2021-06-08 13:05:40 -0500

    cambios en mobile profile

--- src/view/home.js
+++ src/view/home.js
@@ -13,9 +13,9 @@ export default () => {
   const viewHome = `
   <header>
   <h1 class="h1-style"><img src="https://user-images.githubusercontent.com/77282012/118549107-a4ffdc00-b720-11eb-9040-9de50dfb9369.png" alt="app logo">MIURART</h1>
-  <img src="https://user-images.githubusercontent.com/77282012/121115625-f872c080-c7da-11eb-9d55-53aba04edf15.png">
-  <button id="btnSalir">Salir</button>
-</header>
+  <img id="logo" src="https://user-images.githubusercontent.com/77282012/118549107-a4ffdc00-b720-11eb-9040-9de50dfb9369.png" alt="app logo">
+  <button id="btnSalir"><img src="https://user-images.githubusercontent.com/77282012/121115625-f872c080-c7da-11eb-9d55-53aba04edf15.png"></button>
+  </header>
     <section id="viewHome">
     <section id="contenedorMessage"></section>
     <section id="viewPerfil"></section>
@@ -37,12 +37,17 @@ export default () => {
                   <input type="file" accept="image/png, image/jpeg" value="upload" id="fileButton" />
                 </section>
                 <button id="btnPublicar">Publicar</button>
+                <input type="reset" value="Cancelar Post">
               </form>
             </section>
           <section id="postContainer"></section>
         </section>
       </section>
-    </section>`;
+    </section>
+    <footer>
+      <img id="perfilImg" src="https://user-images.githubusercontent.com/67443691/121232034-c2732200-c856-11eb-928c-01e838f3d792.png">
+      <button id="btnSalir"><img src="https://user-images.githubusercontent.com/77282012/121115625-f872c080-c7da-11eb-9d55-53aba04edf15.png"></button>
+    </footer>`;
 
   const divElem = document.createElement('div');
   divElem.innerHTML = viewHome;
@@ -306,50 +311,44 @@ export default () => {
   // references in your storage bucket
   // --ENVIA IMG A FIREBASE AL MOEMNTO DE DAR CLICK ABRIR
 
-  // const preViewImg = (e) => {
-  //   // const storage = firebase.app().storage('gs://miurart---red-social.appspot.com');
-  //   // Create a storage reference from our storage service
-  //   // const storageRef = storage.ref();
-  //   // const fileName;
+  const preViewImg = (e) => {
+    // const storage = firebase.app().storage('gs://miurart---red-social.appspot.com');
+    // Create a storage reference from our storage service
+    // const storageRef = storage.ref();
+    // const fileName;
 
-  //   // btnSelectFile.addEventListener('change', (e) => {
-  //   // const fileName = e.target.files[0].name;
-  //   const reader = new FileReader();
-  //   reader.readAsDataURL(e.target.files[0]);
+    // btnSelectFile.addEventListener('change', (e) => {
+    // const fileName = e.target.files[0].name;
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
 
-  //   reader.onload = function () {
-  //     const preview = divElem.querySelector('#imgpreview');
-  //     const image = document.createElement('img');
+    reader.onload = function () {
+      const preview = divElem.querySelector('#imgpreview');
+      const image = document.createElement('img');
 
-  //     image.src = reader.result;
+      image.src = reader.result;
 
-  //     preview.innerHTML = '';
-  //     preview.append(image);
+      preview.innerHTML = '';
+      preview.append(image);
 
-  //     // return fileName;
-  //     // };
-  //     // console.log(file);
-  //     // const imgpv =
-  //     // divElem.querySelector('#imgpreview').innerHTML = imgpv;
-  //     // const imageRef = storageRef.child(`images/${file.name}`);
-  //     // imageRef.put(file);
-  //   };
-  // };
+      // return fileName;
+      // };
+      // console.log(file);
+      // const imgpv =
+      // divElem.querySelector('#imgpreview').innerHTML = imgpv;
+      // const imageRef = storageRef.child(`images/${file.name}`);
+      // imageRef.put(file);
+    };
+  };
 
   const getFile = (e) => {
     const file = e.target.files[0].name;
-    // btnSelectFile.addEventListener('change', (e) => {
-
-    // console.log(file);
-    // console.log(file);
-    // });
     console.log(file);
     return file;
-    // console.log(fileE());
   };
 
   btnSelectFile.addEventListener('change', (e) => {
-  // preViewImg(e);
+    preViewImg(e);
     getFile(e);
   });
   // uploadImg();
